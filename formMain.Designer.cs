97c469d27fefe8b47e2a274959617884c906030d
SergeiG <sergeig888@hotmail.com>
2021-03-23 13:57:11 -0700

    Init commit

---
+++ formMain.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace PBI_WorkspaceCapacityMover
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.lblTenantId = new System.Windows.Forms.Label();
+            this.tbTenantId = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.btnGetMyWorkspaces = new System.Windows.Forms.Button();
+            this.dgViewMyWorkspaces = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.WorkspaceName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.User = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.State = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CapacityId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbCapacities = new System.Windows.Forms.ComboBox();
+            this.lblCapacities = new System.Windows.Forms.Label();
+            this.btnAssignToCapacity = new System.Windows.Forms.Button();
+            this.tbCapacityInfo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgViewMyWorkspaces)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTenantId
+            // 
+            this.lblTenantId.AutoSize = true;
+            this.lblTenantId.Location = new System.Drawing.Point(3, 16);
+            this.lblTenantId.Name = "lblTenantId";
+            this.lblTenantId.Size = new System.Drawing.Size(58, 13);
+            this.lblTenantId.TabIndex = 0;
+            this.lblTenantId.Text = "Tenant ID:";
+            // 
+            // tbTenantId
+            // 
+            this.tbTenantId.Location = new System.Drawing.Point(83, 13);
+            this.tbTenantId.Name = "tbTenantId";
+            this.tbTenantId.ReadOnly = true;
+            this.tbTenantId.Size = new System.Drawing.Size(269, 20);
+            this.tbTenantId.TabIndex = 1;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(371, 11);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogIn.TabIndex = 2;
+            this.btnLogIn.Text = "Login";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // btnGetMyWorkspaces
+            // 
+            this.btnGetMyWorkspaces.Location = new System.Drawing.Point(463, 11);
+            this.btnGetMyWorkspaces.Name = "btnGetMyWorkspaces";
+            this.btnGetMyWorkspaces.Size = new System.Drawing.Size(127, 23);
+            this.btnGetMyWorkspaces.TabIndex = 3;
+            this.btnGetMyWorkspaces.Text = "Get MyWorkspaces";
+            this.btnGetMyWorkspaces.UseVisualStyleBackColor = true;
+            this.btnGetMyWorkspaces.Click += new System.EventHandler(this.btnGetMyWorkspaces_Click);
+            // 
+            // dgViewMyWorkspaces
+            // 
+            this.dgViewMyWorkspaces.AllowUserToAddRows = false;
+            this.dgViewMyWorkspaces.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgViewMyWorkspaces.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgViewMyWorkspaces.ColumnHeadersHeight = 21;
+            this.dgViewMyWorkspaces.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgViewMyWorkspaces.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.WorkspaceName,
+            this.User,
+            this.State,
+            this.CapacityId});
+            this.dgViewMyWorkspaces.Location = new System.Drawing.Point(6, 73);
+            this.dgViewMyWorkspaces.Name = "dgViewMyWorkspaces";
+            this.dgViewMyWorkspaces.ReadOnly = true;
+            this.dgViewMyWorkspaces.Size = new System.Drawing.Size(1023, 511);
+            this.dgViewMyWorkspaces.TabIndex = 4;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Width = 220;
+            // 
+            // WorkspaceName
+            // 
+            this.WorkspaceName.HeaderText = "Workspace Name";
+            this.WorkspaceName.Name = "WorkspaceName";
+            this.WorkspaceName.ReadOnly = true;
+            this.WorkspaceName.Width = 255;
+            // 
+            // User
+            // 
+            this.User.HeaderText = "User";
+            this.User.Name = "User";
+            this.User.ReadOnly = true;
+            this.User.Width = 205;
+            // 
+            // State
+            // 
+            this.State.HeaderText = "State";
+            this.State.Name = "State";
+            this.State.ReadOnly = true;
+            this.State.Width = 80;
+            // 
+            // CapacityId
+            // 
+            this.CapacityId.HeaderText = "CapacityId";
+            this.CapacityId.Name = "CapacityId";
+            this.CapacityId.ReadOnly = true;
+            this.CapacityId.Width = 220;
+            // 
+            // cbCapacities
+            // 
+            this.cbCapacities.FormattingEnabled = true;
+            this.cbCapacities.Items.AddRange(new object[] {
+            "00000000-0000-0000-0000-000000000000"});
+            this.cbCapacities.Location = new System.Drawing.Point(83, 39);
+            this.cbCapacities.Name = "cbCapacities";
+            this.cbCapacities.Size = new System.Drawing.Size(269, 21);
+            this.cbCapacities.TabIndex = 5;
+            this.cbCapacities.SelectedValueChanged += new System.EventHandler(this.cbCapacities_SelectedValueChanged);
+            // 
+            // lblCapacities
+            // 
+            this.lblCapacities.AutoSize = true;
+            this.lblCapacities.Location = new System.Drawing.Point(3, 42);
+            this.lblCapacities.Name = "lblCapacities";
+            this.lblCapacities.Size = new System.Drawing.Size(59, 13);
+            this.lblCapacities.TabIndex = 6;
+            this.lblCapacities.Text = "Capacities:";
+            // 
+            // btnAssignToCapacity
+            // 
+            this.btnAssignToCapacity.Location = new System.Drawing.Point(371, 40);
+            this.btnAssignToCapacity.Name = "btnAssignToCapacity";
+            this.btnAssignToCapacity.Size = new System.Drawing.Size(219, 23);
+            this.btnAssignToCapacity.TabIndex = 7;
+            this.btnAssignToCapacity.Text = "Assign Selected To Capacity";
+            this.btnAssignToCapacity.UseVisualStyleBackColor = true;
+            this.btnAssignToCapacity.Click += new System.EventHandler(this.btnAssignToCapacity_Click);
+            // 
+            // tbCapacityInfo
+            // 
+            this.tbCapacityInfo.Location = new System.Drawing.Point(608, 12);
+            this.tbCapacityInfo.Multiline = true;
+            this.tbCapacityInfo.Name = "tbCapacityInfo";
+            this.tbCapacityInfo.ReadOnly = true;
+            this.tbCapacityInfo.Size = new System.Drawing.Size(421, 51);
+            this.tbCapacityInfo.TabIndex = 8;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 590);
+            this.Controls.Add(this.tbCapacityInfo);
+            this.Controls.Add(this.btnAssignToCapacity);
+            this.Controls.Add(this.lblCapacities);
+            this.Controls.Add(this.cbCapacities);
+            this.Controls.Add(this.dgViewMyWorkspaces);
+            this.Controls.Add(this.btnGetMyWorkspaces);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.tbTenantId);
+            this.Controls.Add(this.lblTenantId);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1050, 1000);
+            this.MinimumSize = new System.Drawing.Size(1050, 350);
+            this.Name = "formMain";
+            this.Text = "Power BI Workspace Capacity Manager Tool (Copyright © 2021 Sergei Gundorov)";
+            ((System.ComponentModel.ISupportInitialize)(this.dgViewMyWorkspaces)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTenantId;
+        private System.Windows.Forms.TextBox tbTenantId;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Button btnGetMyWorkspaces;
+        private System.Windows.Forms.DataGridView dgViewMyWorkspaces;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn WorkspaceName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn User;
+        private System.Windows.Forms.DataGridViewTextBoxColumn State;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CapacityId;
+        private System.Windows.Forms.ComboBox cbCapacities;
+        private System.Windows.Forms.Label lblCapacities;
+        private System.Windows.Forms.Button btnAssignToCapacity;
+        private System.Windows.Forms.TextBox tbCapacityInfo;
+    }
+}
+
---
+++ formMain.cs
@@ -0,0 +1,300 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.PowerBI.Api;
+using Microsoft.PowerBI.Api.Models;
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBI_WorkspaceCapacityMover
+{
+    public partial class formMain : Form
+    {
+        private static readonly string Username = ConfigurationManager.AppSettings["pbiUsername"];
+        private static readonly string AuthorityUrl = ConfigurationManager.AppSettings["authorityUrl"];
+        private static readonly string ResourceUrl = ConfigurationManager.AppSettings["resourceUrl"];
+        private static readonly string ClientId = ConfigurationManager.AppSettings["clientId"];
+        private static readonly string ApiUrl = ConfigurationManager.AppSettings["apiUrl"];
+        //NOTE: required only for AAD auth prompt option
+        private static readonly Uri RedirectUri = new Uri(ConfigurationManager.AppSettings["redirectUri"]);
+
+        PowerBIClient client;
+        Capacities capacitiesList;
+
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            var task = AthenticateToService();
+        }
+
+        /// <summary>
+        /// Authentication to Power BI API and construction of client object
+        /// </summary>
+        /// <returns>asyncronous task</returns>
+        private async Task AthenticateToService()
+        {
+            // Authenticate using created credentials
+            var authenticationContext = new AuthenticationContext(AuthorityUrl);
+            //NOTE: auth option with creds stored
+            //var authenticationResult = await authenticationContext.AcquireTokenAsync(ResourceUrl, ClientId, credential);
+            //NOTE: auth with a prompt
+            var authenticationResult = await authenticationContext.AcquireTokenAsync(ResourceUrl, ClientId, RedirectUri,
+                new PlatformParameters(PromptBehavior.Auto),
+                new UserIdentifier(Username, UserIdentifierType.OptionalDisplayableId));
+
+            if (authenticationResult == null)
+            {
+                MessageBox.Show("Authentication Failed");
+            }
+
+            var tokenCredentials = new TokenCredentials(authenticationResult.AccessToken, "Bearer");
+
+            //Create a Power BI Client object. It will be used to call Power BI APIs.            
+            client = new PowerBIClient(new Uri(ApiUrl), tokenCredentials);
+
+            //use to test auth issues
+            tbTenantId.Text = authenticationResult.TenantId;
+
+            //populating capacities combo box
+            getCapacitiesList();
+        }
+
+        private async void getCapacitiesList()
+        {
+            try
+            {
+                var capacitiesList = await client.Capacities.GetCapacitiesAsync();
+
+                if (capacitiesList == null || capacitiesList.Value.Count == 0) return;
+
+                foreach (var capacity in capacitiesList.Value)
+                {
+                    cbCapacities.Items.Add(capacity.Id);
+                }
+
+                cbCapacities.SelectedIndex = 0;
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error Getting Capacities", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                updateItemStatus("Get capacities request failed");
+            }
+        }
+
+        private async void btnGetMyWorkspaces_Click(object sender, EventArgs e)
+        {
+            //clearing previos result set
+            dgViewMyWorkspaces.Rows.Clear();
+
+            //enumerating groups
+            //NOTE: need to handle API output in blocks of 5000 calls; tenant is limited to 200 calls per hour
+            //test block size needs to be equal to the number of available
+            //workspaces to test edge case where block size is equal to the number of returned rows 
+            //int blockSize = 611; 
+            int blockSize = 5000;  //largest block size per docs; keep at max to avoid being throttled
+            int blockOffset = 0;
+            int rowCount;
+
+            do
+            {
+                var groups = await client.Groups.GetGroupsAsAdminAsync(blockSize, skip: blockOffset, expand: "users", filter: "type eq 'PersonalGroup'");
+
+                if (groups == null || groups.Value.Count == 0) return;
+
+                rowCount = groups.Value.Count;
+
+                dgViewMyWorkspaces.Rows.Add(rowCount);
+
+                for (int i = blockOffset; i < dgViewMyWorkspaces.Rows.Count; i++)
+                {
+                    dgViewMyWorkspaces.Rows[i].Cells[0].Value = groups.Value[i - blockOffset].Id;
+                    dgViewMyWorkspaces.Rows[i].Cells[1].Value = groups.Value[i - blockOffset].Name;
+                    dgViewMyWorkspaces.Rows[i].Cells[2].Value = groups.Value[i - blockOffset].Users[0].Identifier;
+                    dgViewMyWorkspaces.Rows[i].Cells[3].Value = groups.Value[i - blockOffset].State;
+                    dgViewMyWorkspaces.Rows[i].Cells[4].Value = groups.Value[i - blockOffset].CapacityId.ToString();
+                }
+
+                //intermediate stage update check
+                //System.Diagnostics.Debug.WriteLine("Rows:" + dgViewMyWorkspaces.Rows.Count);
+
+                blockOffset += blockSize;
+
+            } while (rowCount == blockSize);
+
+            //final list update check
+            //System.Diagnostics.Debug.WriteLine("Rows:" + dgViewMyWorkspaces.Rows.Count);
+        }
+
+        private void btnAssignToCapacity_Click(object sender, EventArgs e)
+        {
+            var selectionCount = dgViewMyWorkspaces.SelectedRows.Count;
+            List<string> workspaceGuids = new List<string>();
+
+            if (selectionCount == 0)
+            {
+                MessageBox.Show("No rows selected!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }           
+
+            for (int i = 0; i < selectionCount; i++)
+            {
+                workspaceGuids.Add(dgViewMyWorkspaces.SelectedRows[i].Cells[0].Value.ToString());             
+            }
+                        
+            DialogResult userAction = MessageBox.Show("Target capacity: " + cbCapacities.Text +
+                "\nTop of " + workspaceGuids.Count +
+                " selected workspaces:\n" + string.Join("\n", workspaceGuids.Take(10)),
+                "Proceed with assignment?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (userAction != DialogResult.Yes) return;
+
+            //posting action taken notification
+            foreach (DataGridViewRow row in dgViewMyWorkspaces.SelectedRows)
+            {
+                row.Cells[4].Value = "Migration requested";
+            }
+                        
+            //Testing for user selecting "null" capacity to move workspaces to shared
+            if (cbCapacities.Text == "00000000-0000-0000-0000-000000000000") //TODO: <- make sure this test holds true
+            {
+                unAssignWorkspaces(workspaceGuids);
+            }
+            else
+            {
+                assignToCapacity(cbCapacities.Text, workspaceGuids);
+            }
+        }
+
+        /// <summary>
+        /// Helper method to unassign workspaces
+        /// </summary>
+        /// <param name="workspaces"></param>
+        private void unAssignWorkspaces(List<string> workspaces)
+        {
+            try
+            {                
+                //TODO: add sanity check for the list size; need to determine what is the max for the POST
+                UnassignWorkspacesCapacityRequest unAssignRequest = new UnassignWorkspacesCapacityRequest(workspaces);
+                client.Capacities.UnassignWorkspacesFromCapacity(unAssignRequest);
+            }
+
+            catch (HttpOperationException e)
+            {
+                MessageBox.Show(e.Message + "\n\nDetails:\n" + e.Response.Content,
+                    "Error Sending Unassign Request", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                updateItemStatus("Unassign request failed");
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error Sending Unassign Request", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                updateItemStatus("Unassign request failed");
+            }
+        }
+
+        /// <summary>
+        /// Helper method to assign workspaces
+        /// </summary>
+        private void assignToCapacity(string targetCapacity, List<string> workspaces)
+        {
+            try
+            {
+                //building assign to capacity request object
+                CapacityMigrationAssignment migrationAssignment = new CapacityMigrationAssignment(workspaces, targetCapacity);
+
+                List<CapacityMigrationAssignment> migrationAssignmentList = new List<CapacityMigrationAssignment>();
+
+                migrationAssignmentList.Add(migrationAssignment);
+
+                client.Capacities.AssignWorkspacesToCapacity(new AssignWorkspacesToCapacityRequest(migrationAssignmentList));
+            }
+
+            catch (HttpOperationException e)
+            {
+                MessageBox.Show(e.Message + "\n\nDetails:\n" + e.Response.Content,
+                    "Error Sending Assign Request", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                updateItemStatus("Migration request failed");
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error Sending Assign Request", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                updateItemStatus("Migration request failed");
+            }
+        }
+
+        /// <summary>
+        /// Helper method to update item request status
+        /// </summary>
+        /// <param name="requestStatus">status message text</param>
+        private void updateItemStatus(string requestStatus)
+        {
+            //posting action taken notification
+            foreach (DataGridViewRow row in dgViewMyWorkspaces.SelectedRows)
+            {
+                row.Cells[4].Value = requestStatus;
+            }
+        }
+
+
+        private async void cbCapacities_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cbCapacities.Text.Length == 0) return;
+
+            //special handling shared capacity
+            if (cbCapacities.Text == "00000000-0000-0000-0000-000000000000")
+            {
+                tbCapacityInfo.Text = "Target Capacity: Shared";
+                return;
+            }
+
+            //building available to tenant Admin capacities collection
+            //no need to execute expensive admin call if user only wants to unassign workspaces
+            try
+            {
+                if (capacitiesList is null)
+                {
+                    capacitiesList = await client.Admin.GetCapacitiesAsAdminAsync("tenantKey");
+                }
+
+                var selectedCapacity = capacitiesList.Value.Where(capacity => capacity.Id.ToString() == cbCapacities.Text).FirstOrDefault();
+
+                string capacityInfo = "Target Capacity: " + selectedCapacity.DisplayName +
+                                       "\r\nSKU: " + selectedCapacity.Sku +
+                                       "  Region: " + selectedCapacity.Region;
+
+                if (selectedCapacity.TenantKey is null)
+                {
+                    capacityInfo += "\r\nEncryption Key: not set";
+                }
+                else
+                {
+                    capacityInfo = capacityInfo + "\r\nEncryption Key: " + selectedCapacity.TenantKey.Name;
+                }
+
+                tbCapacityInfo.Text = capacityInfo;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error retrieving and setting capacity information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
